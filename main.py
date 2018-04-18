ccecd40aa013d69c99b74e840a7bf36fdccb7b4a
CJ Melegrito <cj@mlgrto.com>
2018-04-18 14:18:54 +1000

    Finalise Spectrum class

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import numpy as np
+from random import uniform
 from Spectrum import Spectrum
 from Simulator import Simulator
 
-data = None
+def random_spectrum():
+	empty = Spectrum()
+	data = empty.data
+	for i in range(len(data)):
+		data[i][1] = uniform(0,1)
+	spectrum = Spectrum(data)
 
-simulator = Simulator()
+if __name__ == '__main__':
+	# Initialise simulation environment
+	simulator = Simulator()
 
-spectrum = Spectrum(data)
+	# Generate random spectrum
+	spectrum = random_spectrum()
 
-simulator.render({
-	'pupil-distance-inter': None,
-	'pupil-distance': None,
-	'pupil-size': None,
-	'spectrum': spectrum,
-})
+	# Render simulation to file
+	simulator.render({
+		'pupil-distance-inter': None,
+		'pupil-distance': None,
+		'pupil-size': None,
+		'spectrum': spectrum,
+	})
